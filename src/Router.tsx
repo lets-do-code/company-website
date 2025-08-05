fd2947458c4be5b5b433b35ab8432b301300a6de
abhijeet <abhijeet@eazotel.com>
2025-08-05 21:00:38 +0530

    first commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import { AppLayout } from './components/app-layout'
-import NotMatch from './pages/NotMatch'
+// import { AppLayout } from './components/app-layout'
+// import NotMatch from './pages/NotMatch'
 import Dashboard from './pages/Dashboard'
-import Sample from './pages/Sample'
-import ComingSoon from './pages/ComingSoon'
+// import Sample from './pages/Sample'
+// import ComingSoon from './pages/ComingSoon'
 
 export default function Router() {
     return (
--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import createGlobe from 'cobe';
 import { cn } from '@/lib/utils';
 
